1083a1b4a5cc1971e66370a12412399326bc9e83
Rahul Hegde <rahulh.hegde@gmail.com>
2023-02-28 22:24:28 -0800

    [feat] added side nav closing

--- src/SideNav.tsx
+++ src/SideNav.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavSection } from "./utilities/types";
 import styles from "./SideNav.module.css";
 
@@ -7,18 +7,31 @@ type SideNavProps = {
 };
 
 function SideNav({ sections }: SideNavProps) {
+	let [displaySideNav, setDisplaySideNav] = useState(false);
+
+	function toggleDisplay() {
+		setDisplaySideNav(!displaySideNav);
+	}
 	return (
-		<div className={styles.wrapper}>
-			{sections.map((section) => {
-				return (
-					<div key={section.header}>
-						<h1 className={styles.header}>{section.header}</h1>
+		<div className={displaySideNav ? styles.wrapper : styles.buttonWrap}>
+			{displaySideNav ? (
+				<div>
+					{sections.map((section) => {
+						return (
+							<div key={section.header}>
+								<h1 className={styles.header}>{section.header}</h1>
+							</div>
+						);
+					})}
+					<div className={styles.closeButton} onClick={toggleDisplay}>
+						<i className="fa fa-close"></i>
 					</div>
-				);
-			})}
-			<div className={styles.closeButton}>
-				<i className="fa fa-close"></i>
-			</div>
+				</div>
+			) : (
+				<div className={styles.openButton} onClick={toggleDisplay}>
+					<i className="fa fa-angle-right"></i>
+				</div>
+			)}
 		</div>
 	);
 }
